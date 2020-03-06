655f0eadf1828421b8ef66161ef3d96fd90ff513
Sheldon Hull <sheldonhull@users.noreply.github.com>
2020-03-06 15:18:30 -0600

    🚧

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 echo "Customized environment"
 
 echo "Copying settings.json"
-ln -sf ~/.vscode/settings.json ~/.config/Code/User/settings.json
-ln -sf ~/.vscode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -sf ~/.vscode/settings.json /User/settings.json
+ln -sf ~/.vscode/keybindings.json /User/keybindings.json
 
 echo "installing git-town"
-dpkg -i app/git-town-amd64.deb
+sudo dpkg -i app/git-town-amd64.deb
