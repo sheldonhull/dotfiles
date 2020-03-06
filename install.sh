1ccf3fe9bcf68422007655677e10561e424efbe2
Sheldon Hull <sheldonhull@users.noreply.github.com>
2020-03-06 15:05:39 -0600

    ➕ visualstudiocode improvements

--- install.sh
+++ install.sh
@@ -2,4 +2,10 @@
 
 
 echo "Customized environment"
-echo "Need to install git town"
+
+echo "Copying settings.json"
+ln -sf ~/.vscode/settings.json ~/.config/Code/User/settings.json
+ln -sf ~/.vscode/keybindings.json ~/.config/Code/User/keybindings.json
+
+echo "installing git-town"
+dpkg -i app/git-town-amd64.deb
