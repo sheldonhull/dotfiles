7bdcbc0033988a1ae8691240a59408d26f9b3716
Sheldon Hull <sheldonhull@users.noreply.github.com>
2020-03-19 13:14:34 -0500

    mend

--- install.sh
+++ install.sh
@@ -12,5 +12,11 @@ ln -sf ~/.vscode/keybindings.json /User/keybindings.json
 bot "installing git-town"
 sudo dpkg -i app/git-town-amd64.deb
 
+bot "installing starship prompt"
+curl -fsSL https://starship.rs/install.sh | bash
+bot "adding starship to default .bashrc"
+echo "eval ""$(starship init bash)""" >> ~/.bashrc && source ~/.bashrc
+bot "initializing"
+starship init bash
 
 ok "finished with dotfiles"
