beb2c055e19a0e7853547d7912c47ea8e019b454
Sheldon Hull <sheldonhull@users.noreply.github.com>
2020-05-18 22:48:43 -0500

    fix: remove ansible from this repo

--- ansible/TGT/launchctl_ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "###############################################################################"
-echo "SSH"
-echo "###############################################################################"
-
-echo ""
-echo "Enable remote login"
-launchctl load -w /System/Library/LaunchDaemons/ssh.plist
--- ansible/bin/install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ansible-galaxy install --role-file requirements.yml
