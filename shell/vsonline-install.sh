6f8ec2f459f25f723beb63e22c6ebc30695169b9
Sheldon Hull <sheldonhull@users.noreply.github.com>
2020-05-26 15:24:57 -0500

    remote settings

--- shell/vsonline-install.sh
+++ shell/vsonline-install.sh
@@ -5,9 +5,9 @@ awesome_header
 
 bot "Customized environment"
 
-bot "Copying settings.json"
-ln -sf ~/.vscode/settings.json /User/settings.json
-ln -sf ~/.vscode/keybindings.json /User/keybindings.json
+# bot "Copying settings.json"
+# ln -sf ~/.vscode/settings.json /User/settings.json
+# ln -sf ~/.vscode/keybindings.json /User/keybindings.json
 
 bot "installing git-town"
 sudo dpkg -i ../linux/app/git-town-amd64.deb
