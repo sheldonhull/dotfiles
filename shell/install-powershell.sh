678aa9b9b4596c613cc8bb221d9a7009dfe9d189
Sheldon Hull <sheldonhull@users.noreply.github.com>
2020-06-09 13:56:42 -0500

    [wip]

--- shell/install-powershell.sh
+++ shell/install-powershell.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 # Download the Microsoft repository GPG keys
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb && sudo apt-get update
+
+# Update the list of products
+# RUN sudo apt-get update
+
 # Enable the "universe" repositories
-sudo add-apt-repository universe
+# RUN sudo add-apt-repository universe
+
 # Install PowerShell
-RUN sudo apt-get install -y powershell
-# Start PowerShell
+echo "initiating installation of powershell"
+sudo apt-get install -y powershell
