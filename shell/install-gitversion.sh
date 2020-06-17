3456b5c2b6566c6b8a5f2fbd115eaa51559aaf6e
Sheldon Hull <sheldonhull@users.noreply.github.com>
2020-06-17 16:53:56 -0500

    [wip] 0.1.0+34

--- shell/install-gitversion.sh
+++ shell/install-gitversion.sh
@@ -1,6 +1,5 @@
 #!/bin/usr/env bash
 
-
 apt update -yq
 apt install -qy pv wget
 
@@ -14,7 +13,8 @@ tarball="$(find . -name "gitversion-debian*.tar.gz")"
 tar -xzf $tarball
 
 chmod +x gitversion
-./gitversion --version
 mv gitversion ~/
+rm $tarball
+echo ">>>> gitversion version: $(~/gitversion /version)"
 
 # https://github.com/GitTools/GitVersion/releases/download/5.3.6/gitversion-debian.9-x64-5.3.6.tar.gz
