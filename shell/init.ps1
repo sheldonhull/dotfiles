1c710464ce0f371fb36ba0cb2363ca688e04b89b
Sheldon Hull <sheldonhull@users.noreply.github.com>
2020-06-18 13:37:23 -0500

    [wip] 0.1.0+38

--- shell/init.ps1
+++ shell/init.ps1
@@ -3,18 +3,41 @@ Write-Host "Setting profile for default pwsh session"
 $Profiles = @(
     $Profile
     '/root/.config/powershell/Microsoft.VSCode_profile.ps1'
+    '/home/codespace/.config/powershell/Microsoft.PowerShell_profile.ps1'
 )
-$Profiles.ForEach{ New-Item -Path $_ -ItemType File -Force }
+$Profiles.ForEach{ New-Item -Path $_ -ItemType File -Force -ErrorAction SilentlyContinue -ErrorVariable err }
 
 
 Write-Host "configuring alias for terraform"
-$Profiles.ForEach{ $p = $_; New-Alias "tf" "terraform" -ErrorAction SilentlyContinue | Out-File -FilePath $p }
+$Profiles.ForEach{
+    try
+    {
+        $p = $_; New-Alias "tf" "terraform" -ErrorAction SilentlyContinue | Out-File -FilePath $p
+    }
+    catch
+    {
+        Write-Warning $_.Exception.InnerException.Message
+    }
+}
 
+Write-Host "Installing Standard Modules"
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module 'InvokeBuild', 'PSFramework', 'AWS.Tools.Installer', 'Set-PSEnv','PSScriptAnalyzer' -Confirm:$false -Force
+Install-Module 'InvokeBuild', 'PSFramework', 'AWS.Tools.Installer', 'Set-PSEnv', 'PSScriptAnalyzer','Pester' -Confirm:$false -Force
 Install-Module PSReadline -Confirm:$false -AllowPrerelease -Force
 Install-Module EditorServicesCommandSuite -Scope CurrentUser -AllowPrerelease -Force
 
 
 $ProfileContent = Get-Content ./profile.ps1 -Raw
-$Profiles.ForEach{ $p = $_; $ProfileContent | Out-File $p -Force }
+$Profiles.ForEach{
+    try
+    {
+        $p = $_; $ProfileContent | Out-File $p -Force -ErrorAction SilentlyContinue
+    }
+    catch
+    {
+        Write-Warning $_.Exception.InnerException.Message
+    }
+}
+
+
+$err | Write-Warning
