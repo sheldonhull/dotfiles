16ec14bc7c3ce193ce6cd39d1e4026a24564535d
Sheldon Hull <sheldonhull@users.noreply.github.com>
2020-06-22 17:28:22 -0500

    [wip] 0.1.0+40

--- shell/install-gitversion.sh
+++ shell/install-gitversion.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/usr/env bash
 
-apt update -yq
-apt install -qy pv wget
+
+sudo apt -yq update
+sudo apt -qy install pv wget
 
 curl -s https://api.github.com/repos/GitTools/GitVersion/releases/latest \
 | grep "browser_download_url.*gitversion\-debian.*\-x64.*\.tar\.gz" \
@@ -12,9 +13,11 @@ curl -s https://api.github.com/repos/GitTools/GitVersion/releases/latest \
 tarball="$(find . -name "gitversion-debian*.tar.gz")"
 tar -xzf $tarball
 
-chmod +x gitversion
-mv gitversion ~/
-rm $tarball
+sudo chmod +x gitversion
+sudo mv gitversion /usr/local/bin
+
+
+sudo rm $tarball
 echo ">>>> gitversion version: $(~/gitversion /version)"
 
 # https://github.com/GitTools/GitVersion/releases/download/5.3.6/gitversion-debian.9-x64-5.3.6.tar.gz
